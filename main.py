4858e70047a396a39dfa6db52caeb28d5493860b
HARUN PEHLİVAN <trcmnhp@hotmail.com>
2022-01-14 01:06:00 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+fig, axs = plt.subplots(2, 1, constrained_layout=True)
+ax = axs[0]
+x = np.arange(ncols)
+y = np.arange(nrows)
+ax.pcolormesh(x, y, Z, shading='auto', vmin=Z.min(), vmax=Z.max())
+_annotate(ax, x, y, "shading='auto'; X, Y, Z: same shape (nearest)")
+
+ax = axs[1]
+x = np.arange(ncols + 1)
+y = np.arange(nrows + 1)
+ax.pcolormesh(x, y, Z, shading='auto', vmin=Z.min(), vmax=Z.max())
+_annotate(ax, x, y, "shading='auto'; X, Y one larger than Z (flat)")
